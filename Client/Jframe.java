7333b9d8c470e5e015aa2159e19ca057eceb33ae
someonelse <someonelse13@gmail.com>
2025-07-20 15:58:14 -0400

    Update Jframe.java

--- Client/Jframe.java
+++ Client/Jframe.java
@@ -4,108 +4,104 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.*;
-import javax.swing.plaf.metal.MetalLookAndFeel;
-
 
 public class Jframe extends client implements ActionListener {
 
     private static JMenuItem menuItem;
-	private JFrame frame;
+    private JFrame frame;
 
-	public Jframe(String args[]) {
-		super();
-		try {
-			sign.signlink.startpriv(InetAddress.getByName(server));
-			initUI();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+    public Jframe(String[] args) {
+        super();
+        try {
+            sign.signlink.startpriv(InetAddress.getByName(server));
+            initUI();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
-	public void initUI() {
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			JPopupMenu.setDefaultLightWeightPopupEnabled(false);
-			frame = new JFrame("StoneGuide | Client Version 1.0");
-			frame.setLayout(new BorderLayout());
-			frame.setResizable(false);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			JPanel gamePanel = new JPanel();
+    public void initUI() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            JPopupMenu.setDefaultLightWeightPopupEnabled(false);
 
-			gamePanel.setLayout(new BorderLayout());
-			gamePanel.add(this);
-			gamePanel.setPreferredSize(new Dimension(765, 503));
+            frame = new JFrame("StoneGuide | Client Version 1.0");
+            frame.setLayout(new BorderLayout());
+            frame.setResizable(false);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-			JMenu fileMenu = new JMenu("Info");
+            JPanel gamePanel = new JPanel(new BorderLayout());
+            gamePanel.add(this);
+            gamePanel.setPreferredSize(new Dimension(765, 503));
 
-			String[] mainButtons = new String[] { "Files", "Version: 1.0", "-", "Forums", "-", "Exit" };
-			for (String name : mainButtons) {
-				JMenuItem menuItem = new JMenuItem(name);
-				if (name.equalsIgnoreCase("-")) {
-					fileMenu.addSeparator();
-				} else {
-					menuItem.addActionListener(this);
-					fileMenu.add(menuItem);
-				}
-			}
+            JMenu fileMenu = new JMenu("Info");
+            String[] mainButtons = { "Files", "Version: 1.0", "-", "Forums", "-", "Exit" };
 
-			JMenuBar menuBar = new JMenuBar();
-			JMenuBar jmenubar = new JMenuBar();
+            for (String name : mainButtons) {
+                if (name.equals("-")) {
+                    fileMenu.addSeparator();
+                } else {
+                    JMenuItem item = new JMenuItem(name);
+                    item.addActionListener(this);
+                    fileMenu.add(item);
+                }
+            }
 
-			frame.add(jmenubar);
-			menuBar.add(fileMenu);
-			frame.getContentPane().add(menuBar, BorderLayout.NORTH);
-			frame.getContentPane().add(gamePanel, BorderLayout.CENTER);
-			frame.pack();
+            JMenuBar menuBar = new JMenuBar();
+            menuBar.add(fileMenu);
+            frame.setJMenuBar(menuBar);
 
-			frame.setVisible(true); // can see the client
-			frame.setResizable(false); // resizeable frame
+            frame.getContentPane().add(gamePanel, BorderLayout.CENTER);
+            frame.pack();
+            frame.setVisible(true);
+            frame.setResizable(false);
 
-			init();
-		} catch (Exception e) {
-				e.printStackTrace();
-		}
-	}
+            init();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public URL getCodeBase() {
-		try {
-			return new URL("http://" + server + "/cache");
-		} catch (Exception e) {
-			return super.getCodeBase();
-		}
-	}
+    @Override
+    public URL getCodeBase() {
+        try {
+            return new URL("http://" + server + "/cache");
+        } catch (Exception e) {
+            return super.getCodeBase();
+        }
+    }
 
-	public URL getDocumentBase() {
-		return getCodeBase();
-	}
+    @Override
+    public URL getDocumentBase() {
+        return getCodeBase();
+    }
 
-	public void loadError(String s) {
-		System.out.println("loadError: " + s);
-	}
+    public void loadError(String s) {
+        System.out.println("loadError: " + s);
+    }
 
-	public String getParameter(String key) {
-			return "";
-	}
+    @Override
+    public String getParameter(String key) {
+        return "";
+    }
 
-	private static void openUpWebSite(String url) {
-		Desktop d = Desktop.getDesktop();
-		try {
-			d.browse(new URI(url)); 	
-		} catch (Exception e) {
-		}
-	}
+    private static void openUpWebSite(String url) {
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (Exception ignored) {}
+    }
 
-	public void actionPerformed(ActionEvent evt) {
-		String cmd = evt.getActionCommand();
-		try {
-			if (cmd != null) {
-				if (cmd.equalsIgnoreCase("Exit Client")) {
-					System.exit(0);
-				}
-				if (cmd.equalsIgnoreCase("Forums")) {
-					openUpWebSite("http://www.comingsoon.tk/");				}	
-			}
-		} catch (Exception e) {
-		}
-	}
+    @Override
+    public void actionPerformed(ActionEvent evt) {
+        String cmd = evt.getActionCommand();
+        try {
+            if (cmd != null) {
+                if (cmd.equalsIgnoreCase("Exit") || cmd.equalsIgnoreCase("Exit Client")) {
+                    System.exit(0);
+                } else if (cmd.equalsIgnoreCase("Forums")) {
+                    openUpWebSite("http://www.comingsoon.tk/");
+                }
+            }
+        } catch (Exception ignored) {}
+    }
 }
