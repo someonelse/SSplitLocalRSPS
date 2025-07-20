bd0bf86dfd65097e301aad3c4fdd5c4c064df401
someonelse <someonelse13@gmail.com>
2025-07-19 22:14:02 -0400

    Update PacketBuilder.java

--- Server/src/server/net/PacketBuilder.java
+++ Server/src/server/net/PacketBuilder.java
@@ -1,10 +1,11 @@
 package server.net;
 
 /**
- * Packet builder interface
+ * Packet builder interface.
+ * Marker for classes that build packets for the server protocol.
+ * 
  * @author Graham
- *
  */
 public interface PacketBuilder {
-
+    // Marker interface for packet builders.
 }
