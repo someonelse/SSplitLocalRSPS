8f1d0b50962cd633c4c30544157e8dee7dcc2036
someonelse <someonelse13@gmail.com>
2025-07-20 14:27:31 -0400

    Update GameItem.java

--- Server/src/server/model/items/GameItem.java
+++ Server/src/server/model/items/GameItem.java
@@ -1,15 +1,26 @@
 package server.model.items;
 
-
 public class GameItem {
-	public int id, amount;
-	public boolean stackable = false;
+
+	private final int id;
+	private final int amount;
+	private final boolean stackable;
 
 	public GameItem(int id, int amount) {
-		if (Item.itemStackable[id]) {
-			stackable = true;
-		}
-	this.id = id;
-	this.amount = amount;
+		this.id = id;
+		this.amount = amount;
+		this.stackable = Item.itemStackable[id];
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public boolean isStackable() {
+		return stackable;
 	}
 }
