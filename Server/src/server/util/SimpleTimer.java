ed9c3cef1ab8b78ccb587d32ee7f71200d2fb09f
someonelse <someonelse13@gmail.com>
2025-07-19 21:59:48 -0400

    Update SimpleTimer.java

--- Server/src/server/util/SimpleTimer.java
+++ Server/src/server/util/SimpleTimer.java
@@ -13,6 +13,40 @@ public class SimpleTimer {
 	}
 
 	public long elapsed() {
-		return System.currentTimeMillis() - cachedTime;
+		package server.util;
+
+/**
+ * A simple utility timer for measuring elapsed time.
+ */
+public class SimpleTimer {
+
+    private long startTime;
+
+    public SimpleTimer() {
+        reset();
+    }
+
+    /**
+     * Resets the timer to the current time.
+     */
+    public void reset() {
+        startTime = System.nanoTime();
+    }
+
+    /**
+     * Returns the elapsed time in milliseconds since the last reset.
+     */
+    public long elapsed() {
+        return (System.nanoTime() - startTime) / 1_000_000L;
+    }
+
+    /**
+     * Returns the elapsed time in nanoseconds since the last reset.
+     */
+    public long elapsedNanos() {
+        return System.nanoTime() - startTime;
+    }
+}
+return System.currentTimeMillis() - cachedTime;
 	}
 }
