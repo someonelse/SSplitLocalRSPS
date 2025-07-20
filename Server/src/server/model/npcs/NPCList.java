3ef48c2f15796021258a037912ec106983174119
someonelse <someonelse13@gmail.com>
2025-07-20 00:13:38 -0400

    Update NPCList.java

--- Server/src/server/model/npcs/NPCList.java
+++ Server/src/server/model/npcs/NPCList.java
@@ -1,11 +1,48 @@
 package server.model.npcs;
+
 public class NPCList {
-	public int npcId;
-	public String npcName;
-	public int npcCombat;
-	public int npcHealth;
-	
-	public NPCList(int _npcId) {
-		npcId = _npcId;
+
+	private final int npcId;
+	private String npcName;
+	private int npcCombat;
+	private int npcHealth;
+
+	public NPCList(int npcId) {
+		this.npcId = npcId;
+	}
+
+	public NPCList(int npcId, String npcName, int npcCombat, int npcHealth) {
+		this.npcId = npcId;
+		this.npcName = npcName;
+		this.npcCombat = npcCombat;
+		this.npcHealth = npcHealth;
+	}
+
+	public int getNpcId() {
+		return npcId;
+	}
+
+	public String getNpcName() {
+		return npcName;
+	}
+
+	public void setNpcName(String npcName) {
+		this.npcName = npcName;
+	}
+
+	public int getNpcCombat() {
+		return npcCombat;
+	}
+
+	public void setNpcCombat(int npcCombat) {
+		this.npcCombat = npcCombat;
+	}
+
+	public int getNpcHealth() {
+		return npcHealth;
+	}
+
+	public void setNpcHealth(int npcHealth) {
+		this.npcHealth = npcHealth;
 	}
 }
