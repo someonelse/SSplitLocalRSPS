57c62b795ad1ae3d90ebf9c41f99b7a54fe1b4ca
someonelse <someonelse13@gmail.com>
2025-07-19 22:57:18 -0400

    Update PacketType.java

--- Server/src/server/model/players/PacketType.java
+++ Server/src/server/model/players/PacketType.java
@@ -1,8 +1,5 @@
 package server.model.players;
 
-
-	
 public interface PacketType {
-	public void processPacket(Client c, int packetType, int packetSize);
+    public void processPacket(Client c, int packetType, int packetSize);
 }
-
