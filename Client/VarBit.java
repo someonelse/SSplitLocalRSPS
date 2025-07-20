3aad9ccc15627d3716ddecf362aed7e5cf0beae6
someonelse <someonelse13@gmail.com>
2025-07-20 14:45:39 -0400

    Update VarBit.java

--- Client/VarBit.java
+++ Client/VarBit.java
@@ -1,65 +1,71 @@
 // Decompiled by Jad v1.5.8f. Copyright 2001 Pavel Kouznetsov.
 // Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) 
+// Decompiler options: packimports(3)
 
 public final class VarBit {
 
-	public static void unpackConfig(StreamLoader streamLoader)
-	{
-		Stream stream = new Stream(streamLoader.getDataForName("varbit.dat"));
-		int cacheSize = stream.readUnsignedWord();
-		if(cache == null)
-			cache = new VarBit[cacheSize];
-		for(int j = 0; j < cacheSize; j++)
-		{
-			if(cache[j] == null)
-				cache[j] = new VarBit();
-			cache[j].readValues(stream);
-			if(cache[j].aBoolean651)
-				Varp.cache[cache[j].anInt648].aBoolean713 = true;
-		}
+    public static VarBit[] cache;
+    public int anInt648;
+    public int anInt649;
+    public int anInt650;
+    private boolean aBoolean651;
 
-		if(stream.currentOffset != stream.buffer.length)
-			System.out.println("varbit load mismatch");
-	}
+    public static void unpackConfig(StreamLoader streamLoader) {
+        Stream stream = new Stream(streamLoader.getDataForName("varbit.dat"));
+        int cacheSize = stream.readUnsignedWord();
 
-	private void readValues(Stream stream)
-	{
-		do
-		{
-			int j = stream.readUnsignedByte();
-			if(j == 0)
-				return;
-			if(j == 1)
-			{
-				anInt648 = stream.readUnsignedWord();
-				anInt649 = stream.readUnsignedByte();
-				anInt650 = stream.readUnsignedByte();
-			} else
-			if(j == 10)
-				stream.readString();
-			else
-			if(j == 2)
-				aBoolean651 = true;
-			else
-			if(j == 3)
-				stream.readDWord();
-			else
-			if(j == 4)
-				stream.readDWord();
-			else
-				System.out.println("Error unrecognised config code: " + j);
-		} while(true);
-	}
+        if (cache == null) {
+            cache = new VarBit[cacheSize];
+        }
 
-	private VarBit()
-	{
-		aBoolean651 = false;
-	}
+        for (int j = 0; j < cacheSize; j++) {
+            if (cache[j] == null) {
+                cache[j] = new VarBit();
+            }
+            cache[j].readValues(stream);
+            if (cache[j].aBoolean651) {
+                Varp.cache[cache[j].anInt648].aBoolean713 = true;
+            }
+        }
 
-	public static VarBit cache[];
-	public int anInt648;
-	public int anInt649;
-	public int anInt650;
-	private boolean aBoolean651;
+        if (stream.currentOffset != stream.buffer.length) {
+            System.out.println("varbit load mismatch");
+        }
+    }
+
+    private void readValues(Stream stream) {
+        while (true) {
+            int j = stream.readUnsignedByte();
+            if (j == 0) {
+                return;
+            }
+
+            switch (j) {
+                case 1:
+                    anInt648 = stream.readUnsignedWord();
+                    anInt649 = stream.readUnsignedByte();
+                    anInt650 = stream.readUnsignedByte();
+                    break;
+                case 2:
+                    aBoolean651 = true;
+                    break;
+                case 3:
+                    stream.readDWord(); // unused
+                    break;
+                case 4:
+                    stream.readDWord(); // unused
+                    break;
+                case 10:
+                    stream.readString(); // unused string
+                    break;
+                default:
+                    System.out.println("Error unrecognised config code: " + j);
+                    break;
+            }
+        }
+    }
+
+    private VarBit() {
+        this.aBoolean651 = false;
+    }
 }
