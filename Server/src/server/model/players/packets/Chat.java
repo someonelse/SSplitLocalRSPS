8ba076eede2acbde637e36ff6f6b5e332d89ffe2
someonelse <someonelse13@gmail.com>
2025-07-20 00:03:33 -0400

    Update Chat.java

--- Server/src/server/model/players/packets/Chat.java
+++ Server/src/server/model/players/packets/Chat.java
@@ -1,5 +1,5 @@
-
 package server.model.players.packets;
+
 import server.model.players.Player;
 import server.model.players.PlayerHandler;
 import server.model.players.Client;
@@ -8,18 +8,25 @@ import server.Connection;
 import server.Config;
 
 /**
- * Chat
- **/
+ * Handles player chat input
+ */
 public class Chat implements PacketType {
 
-	@Override
-	public void processPacket(Client c, int packetType, int packetSize) {
+    @Override
+    public void processPacket(Client c, int packetType, int packetSize) {
+        // Read chat text effects and color
+        c.setChatTextEffects(c.getInStream().readUnsignedByteS());
+        c.setChatTextColor(c.getInStream().readUnsignedByteS());
 
-		c.setChatTextEffects(c.getInStream().readUnsignedByteS());
-		c.setChatTextColor(c.getInStream().readUnsignedByteS());
-        c.setChatTextSize((byte)(c.packetSize - 2));
+        // Determine chat message size
+        c.setChatTextSize((byte) (c.packetSize - 2));
+
+        // Read the actual chat message into buffer
         c.inStream.readBytes_reverseA(c.getChatText(), c.getChatTextSize(), 0);
-		if (!Connection.isMuted(c))
-			c.setChatTextUpdateRequired(true);
-	}	
+
+        // If player is not muted, mark chat update as needed
+        if (!Connection.isMuted(c)) {
+            c.setChatTextUpdateRequired(true);
+        }
+    }
 }
