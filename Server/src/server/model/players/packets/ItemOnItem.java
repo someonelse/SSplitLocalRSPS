40b49f74d8f888d77f2bdab2a6c406d5f5c4ff23
someonelse <someonelse13@gmail.com>
2025-07-19 23:34:28 -0400

    Update ItemOnItem.java

--- Server/src/server/model/players/packets/ItemOnItem.java
+++ Server/src/server/model/players/packets/ItemOnItem.java
@@ -1,6 +1,10 @@
 package server.model.players.packets;
 
 /**
+ * Handles when a player uses one item on another in their inventory.
+ * 
+ * Example: Using a needle on leather, or combining two items.
+ * 
  * @author Ryan / Lmctruck30
  */
 
@@ -10,13 +14,22 @@ import server.model.players.PacketType;
 
 public class ItemOnItem implements PacketType {
 
-	@Override
-	public void processPacket(Client c, int packetType, int packetSize) {
-		int usedWithSlot = c.getInStream().readUnsignedWord();
-		int itemUsedSlot = c.getInStream().readUnsignedWordA();
-		int useWith = c.playerItems[usedWithSlot] - 1;
-		int itemUsed = c.playerItems[itemUsedSlot] - 1;
-		UseItem.ItemonItem(c, itemUsed, useWith);
-	}
+    @Override
+    public void processPacket(Client c, int packetType, int packetSize) {
+        int usedWithSlot = c.getInStream().readUnsignedWord();
+        int itemUsedSlot = c.getInStream().readUnsignedWordA();
 
+        if (usedWithSlot < 0 || usedWithSlot >= c.playerItems.length || itemUsedSlot < 0 || itemUsedSlot >= c.playerItems.length) {
+            return; // Prevent out-of-bounds errors
+        }
+
+        int usedWithItemId = c.playerItems[usedWithSlot] - 1;
+        int itemUsedId = c.playerItems[itemUsedSlot] - 1;
+
+        if (usedWithItemId < 0 || itemUsedId < 0) {
+            return; // Invalid item IDs
+        }
+
+        UseItem.ItemonItem(c, itemUsedId, usedWithItemId);
+    }
 }
