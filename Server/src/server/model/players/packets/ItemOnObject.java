f471c036e48e017ced1dc3067c1c46b168d96b27
someonelse <someonelse13@gmail.com>
2025-07-19 23:33:22 -0400

    Update ItemOnObject.java

--- Server/src/server/model/players/packets/ItemOnObject.java
+++ Server/src/server/model/players/packets/ItemOnObject.java
@@ -1,30 +1,26 @@
 package server.model.players.packets;
 
-/**
- * @author Ryan / Lmctruck30
- */
-
 import server.model.items.UseItem;
 import server.model.players.Client;
 import server.model.players.PacketType;
 
+/**
+ * Handles when an item is used on an object.
+ * For example: using a seed on a farming patch, or logs on a fire.
+ * 
+ * @author Ryan / Lmctruck30
+ */
 public class ItemOnObject implements PacketType {
 
-	@Override
-	public void processPacket(Client c, int packetType, int packetSize) {
-		/*
-		 * a = ?
-		 * b = ?
-		 */
-		
-		int a = c.getInStream().readUnsignedWord();
-		int objectId = c.getInStream().readSignedWordBigEndian();
-		int objectY = c.getInStream().readSignedWordBigEndianA();
-		int b = c.getInStream().readUnsignedWord();
-		int objectX = c.getInStream().readSignedWordBigEndianA();
-		int itemId = c.getInStream().readUnsignedWord();
-		UseItem.ItemonObject(c, objectId, objectX, objectY, itemId);
-		
-	}
+    @Override
+    public void processPacket(Client c, int packetType, int packetSize) {
+        int a = c.getInStream().readUnsignedWord(); // unknown/unused
+        int objectId = c.getInStream().readSignedWordBigEndian();
+        int objectY = c.getInStream().readSignedWordBigEndianA();
+        int b = c.getInStream().readUnsignedWord(); // unknown/unused
+        int objectX = c.getInStream().readSignedWordBigEndianA();
+        int itemId = c.getInStream().readUnsignedWord();
 
+        UseItem.ItemonObject(c, objectId, objectX, objectY, itemId);
+    }
 }
