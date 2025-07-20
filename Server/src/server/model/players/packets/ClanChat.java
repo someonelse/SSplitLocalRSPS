c9c221056ea9df5715faa3f1f998e5da7ab0b769
someonelse <someonelse13@gmail.com>
2025-07-20 00:03:04 -0400

    Update ClanChat.java

--- Server/src/server/model/players/packets/ClanChat.java
+++ Server/src/server/model/players/packets/ClanChat.java
@@ -4,16 +4,20 @@ import server.util.Misc;
 import server.model.players.Client;
 import server.model.players.PacketType;
 import server.Server;
+
 /**
- * Chat
- **/
+ * Handles joining clan chat
+ */
 public class ClanChat implements PacketType {
 
-	@Override
-	public void processPacket(Client c, int packetType, int packetSize) {
-		String textSent = Misc.longToPlayerName2(c.getInStream().readQWord());
-		textSent = textSent.replaceAll("_", " ");
-		//c.sendMessage(textSent);
-		Server.clanChat.handleClanChat(c, textSent);
-	}	
+    @Override
+    public void processPacket(Client c, int packetType, int packetSize) {
+        String textSent = Misc.longToPlayerName2(c.getInStream().readQWord());
+        textSent = textSent.replace("_", " ");
+        
+        // Original debug/test line:
+        // c.sendMessage(textSent);
+
+        Server.clanChat.handleClanChat(c, textSent);
+    }
 }
