9adae20062d0aaec0de3994593f57d020f94948a
someonelse <someonelse13@gmail.com>
2025-07-19 22:36:38 -0400

    Update EconomyReset.java

--- Server/EconomyReset.java
+++ Server/EconomyReset.java
@@ -4,55 +4,54 @@ import java.io.FileOutputStream;
 import java.io.DataInputStream;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.IOException;
 
-public class EconomyReset
-{
+public class EconomyReset {
 
-        private static File charDir = new File("Data/characters/");
+    private static final File charDir = new File("Data/characters/");
 
-        public static void main(String[] args)
-        {
-                if(charDir.exists() && charDir.isDirectory())
-                {
-                        File[] charFiles = charDir.listFiles();
-                for(int i = 0; i < charFiles.length; i++)
-                {
-                        resetEcoChar(charFiles[i]);
-                        System.out.println("Reset player economy levels for... "+charFiles[i].toString());
-                        }
+    public static void main(String[] args) {
+        if (charDir.exists() && charDir.isDirectory()) {
+            File[] charFiles = charDir.listFiles();
+            if (charFiles != null) {
+                for (File charFile : charFiles) {
+                    resetEcoChar(charFile);
+                    System.out.println("Reset player economy levels for... " + charFile);
                 }
+            }
         }
+    }
 
-        private static void resetEcoChar(File charFile)
-        {
-                try
-                {
+    private static void resetEcoChar(File charFile) {
+        try {
+            String tempData, tempAdd = "";
+            int curEquip = 0;
 
-                        String cheatStatus, tempData, tempAdd = ""; int curEquip = 0, curItem = 0, curBank = 0;
-                        File tempCharFile = new File(charDir.toString()+"ECOBOOST$TEMP");
-                        DataInputStream fileStream = new DataInputStream(new FileInputStream(charFile));
-                        BufferedWriter tempOut = new BufferedWriter(new FileWriter(tempCharFile));
+            File tempCharFile = new File(charDir + "ECOBOOST$TEMP");
+            DataInputStream fileStream = new DataInputStream(new FileInputStream(charFile));
+            BufferedWriter tempOut = new BufferedWriter(new FileWriter(tempCharFile));
 
-                while((tempData = fileStream.readLine()) != null)
-                {
-                        if((!tempData.trim().startsWith("character-item =")) && (!tempData.trim().startsWith("character-bank =")))
-                        {
-                                tempAdd = tempData.trim();
+            while ((tempData = fileStream.readLine()) != null) {
+                tempData = tempData.trim();
+                if (!tempData.startsWith("character-item =") && !tempData.startsWith("character-bank =")) {
+                    tempAdd = tempData;
 
-                                if(tempData.trim().startsWith("character-equip =")) 
-                                {
-                                        tempAdd = "character-equip = "+curEquip+"\t-1\t0";
-                                        curEquip++;
-                                }
-                        tempOut.write(tempAdd); tempOut.newLine();
-                                }
-                        }
-                                fileStream.close(); tempOut.close();
-                                charFile.delete();
-                                tempCharFile.renameTo(charFile);
-                        }
-                catch(Exception e) { e.printStackTrace(); 
+                    if (tempData.startsWith("character-equip =")) {
+                        tempAdd = "character-equip = " + curEquip + "\t-1\t0";
+                        curEquip++;
+                    }
+                    tempOut.write(tempAdd);
+                    tempOut.newLine();
                 }
+            }
+            fileStream.close();
+            tempOut.close();
+
+            charFile.delete();
+            tempCharFile.renameTo(charFile);
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
 }
-
