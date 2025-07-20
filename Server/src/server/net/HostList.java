896349ff9cc52781bfb2e58cf55f49ba129cb552
someonelse <someonelse13@gmail.com>
2025-07-19 22:15:16 -0400

    Update HostList.java

--- Server/src/server/net/HostList.java
+++ Server/src/server/net/HostList.java
@@ -9,50 +9,59 @@ import org.apache.mina.common.IoSession;
 import server.Config;
 import server.Connection;
 
+/**
+ * Keeps track of per-host connections for session limiting.
+ */
 public class HostList {
-	
-	private static HostList list = new HostList();
-	
-	public static HostList getHostList() {
-		return list;
-	}
-	
-	private Map<String, Integer> connections = new HashMap<String, Integer>();
-	
-	public synchronized boolean add(IoSession session) {
-		String addr = ((InetSocketAddress) session.getRemoteAddress()).getAddress().getHostAddress();
-		Integer amt = connections.get(addr);
-		if(amt == null) {
-			amt = 1;
-		} else {
-			amt += 1;
-		}
-		if(amt == 1337) {
-			return false;
-		}
-		if(amt > Config.IPS_ALLOWED || Connection.isIpBanned(addr)) {
-			return false;
-		} else {
-			connections.put(addr, amt);
-			return true;
-		}
-	}
-	
-	public synchronized void remove(IoSession session) {
-		if(session.getAttribute("inList") != Boolean.TRUE) {
-			return;
-		}
-		String addr = ((InetSocketAddress) session.getRemoteAddress()).getAddress().getHostAddress();
-		Integer amt = connections.get(addr);
-		if(amt == null) {
-			return;
-		}
-		amt -= 1;
-		if(amt <= 0) {
-			connections.remove(addr);
-		} else {
-			connections.put(addr, amt);
-		}
-	}
 
+    private static final HostList INSTANCE = new HostList();
+
+    public static HostList getHostList() {
+        return INSTANCE;
+    }
+
+    private final Map<String, Integer> connections = new HashMap<>();
+
+    /**
+     * Adds a session's remote host to the connection count.
+     * @param session The IoSession to track
+     * @return true if the host is allowed to connect, false if blocked/banned/limited
+     */
+    public synchronized boolean add(IoSession session) {
+        String addr = ((InetSocketAddress) session.getRemoteAddress()).getAddress().getHostAddress();
+        int amt = connections.getOrDefault(addr, 0) + 1;
+
+        // "1337" check: some sort of 'special' lockout?
+        if (amt == 1337) {
+            return false;
+        }
+
+        if (amt > Config.IPS_ALLOWED || Connection.isIpBanned(addr)) {
+            return false;
+        }
+
+        connections.put(addr, amt);
+        return true;
+    }
+
+    /**
+     * Removes a session's host from the connection count.
+     * @param session The IoSession to remove
+     */
+    public synchronized void remove(IoSession session) {
+        if (session.getAttribute("inList") != Boolean.TRUE) {
+            return;
+        }
+        String addr = ((InetSocketAddress) session.getRemoteAddress()).getAddress().getHostAddress();
+        Integer amt = connections.get(addr);
+        if (amt == null) {
+            return;
+        }
+        amt--;
+        if (amt <= 0) {
+            connections.remove(addr);
+        } else {
+            connections.put(addr, amt);
+        }
+    }
 }
