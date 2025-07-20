05ce9cfc0ab661369f7d3578331a3b68f63b7771
someonelse <someonelse13@gmail.com>
2025-07-20 15:49:19 -0400

    Update MouseDetection.java

--- Client/MouseDetection.java
+++ Client/MouseDetection.java
@@ -2,44 +2,38 @@
 // Jad home page: http://www.kpdus.com/jad.html
 // Decompiler options: packimports(3) 
 
-final class MouseDetection
-		implements Runnable
-{
+final class MouseDetection implements Runnable {
 
-	public void run()
-	{
-		while(running) 
-		{
-			synchronized(syncObject)
-			{
-				if(coordsIndex < 500)
-				{
-					coordsX[coordsIndex] = clientInstance.mouseX;
-					coordsY[coordsIndex] = clientInstance.mouseY;
-					coordsIndex++;
-				}
-			}
-			try
-			{
-				Thread.sleep(50L);
-			}
-			catch(Exception _ex) { }
-		}
-	}
+    @Override
+    public void run() {
+        while (running) {
+            synchronized (syncObject) {
+                if (coordsIndex < 500) {
+                    coordsX[coordsIndex] = clientInstance.mouseX;
+                    coordsY[coordsIndex] = clientInstance.mouseY;
+                    coordsIndex++;
+                }
+            }
+            try {
+                Thread.sleep(50L);
+            } catch (Exception _ex) {
+                // Ignored
+            }
+        }
+    }
 
-	public MouseDetection(client client1)
-	{
-		syncObject = new Object();
-		coordsY = new int[500];
-		running = true;
-		coordsX = new int[500];
-		clientInstance = client1;
-	}
+    public MouseDetection(client client1) {
+        syncObject = new Object();
+        coordsY = new int[500];
+        running = true;
+        coordsX = new int[500];
+        clientInstance = client1;
+    }
 
-	private client clientInstance;
-	public final Object syncObject;
-	public final int[] coordsY;
-	public boolean running;
-	public final int[] coordsX;
-	public int coordsIndex;
+    private final client clientInstance;
+    public final Object syncObject;
+    public final int[] coordsY;
+    public final int[] coordsX;
+    public boolean running;
+    public int coordsIndex;
 }
