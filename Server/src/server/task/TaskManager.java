ea0d72818f69fdffca61a12853752344801c1771
someonelse <someonelse13@gmail.com>
2025-07-19 22:06:01 -0400

    Update TaskManager.java

--- Server/src/server/task/TaskManager.java
+++ Server/src/server/task/TaskManager.java
@@ -9,29 +9,34 @@ import server.task.TaskFactory.Task;
 
 public class TaskManager {
 
-	/*
-	 * Note: The pool size _must_ remain 1 to keep synchronization proper.
-	 */
-	private static ScheduledThreadPoolExecutor scheduledExecutor = (ScheduledThreadPoolExecutor) Executors
-			.newScheduledThreadPool(1);
+    /*
+     * Note: The pool size _must_ remain 1 to keep synchronization proper.
+     */
+    private static final ScheduledThreadPoolExecutor scheduledExecutor = 
+        (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(1);
 
-	public static Future<?> registerClientTask(Task task, int delay) {
-		Future<?> taskFuture = TaskManager.scheduledExecutor.schedule(task,
-				delay, TimeUnit.SECONDS);
-		if (!task.isGlobal())
-			task.getClient().setCurrentTask(taskFuture);
-		return taskFuture;
-	}
+    /**
+     * Registers a client task to run after a specified delay in seconds.
+     * If the task is not global, it sets the current task in the client.
+     */
+    public static Future<?> registerClientTask(Task task, int delay) {
+        Future<?> taskFuture = scheduledExecutor.schedule(task, delay, TimeUnit.SECONDS);
+        if (!task.isGlobal())
+            task.getClient().setCurrentTask(taskFuture);
+        return taskFuture;
+    }
 
-	public static Future<?> registerDelayedTask(Runnable task, int delay,
-			TimeUnit unit) {
-		return TaskManager.scheduledExecutor.schedule(task, delay, unit);
-	}
+    /**
+     * Registers a delayed task with the specified time unit.
+     */
+    public static Future<?> registerDelayedTask(Runnable task, int delay, TimeUnit unit) {
+        return scheduledExecutor.schedule(task, delay, unit);
+    }
 
-	public static Future<?> scheduleTask(Runnable task, int delay, int rate,
-			TimeUnit unit) {
-		return TaskManager.scheduledExecutor.scheduleWithFixedDelay(task,
-				delay, rate, unit);
-	}
-
+    /**
+     * Schedules a recurring task with fixed delay.
+     */
+    public static Future<?> scheduleTask(Runnable task, int delay, int rate, TimeUnit unit) {
+        return scheduledExecutor.scheduleWithFixedDelay(task, delay, rate, unit);
+    }
 }
