9006a6547c94ec4a169b2e79e233c498f0c84d4e
someonelse <someonelse13@gmail.com>
2025-07-19 22:58:10 -0400

    Update Food.java

--- Server/src/server/model/players/Food.java
+++ Server/src/server/model/players/Food.java
@@ -8,26 +8,27 @@ import java.util.HashMap;
 
 public class Food {
 	
-	
 	private Client c;
 	
-	public Food (Client c) {
+	public Food(Client c) {
 		this.c = c;	
 	}
+	
 	public static enum FoodToEat {		
-		MANTA(391,22,"Manta Ray"),
-		SHARK(385,20,"Shark"),
-		LOBSTER(379,12,"Lobster"),
-		TROUT(333,7,"Trout"),
-		SALMON(329,9,"Salmon"),
-		SWORDFISH(373,14,"Swordfish"),
-		TUNA(361,10,"Tuna"),
-		MONKFISH(7946,16,"Monkfish"),
-		SEA_TURTLE(397,22,"Sea Turtle"),
-		TUNA_POTATO(7060,22,"Tuna Potato");
+		MANTA(391, 22, "Manta Ray"),
+		SHARK(385, 20, "Shark"),
+		LOBSTER(379, 12, "Lobster"),
+		TROUT(333, 7, "Trout"),
+		SALMON(329, 9, "Salmon"),
+		SWORDFISH(373, 14, "Swordfish"),
+		TUNA(361, 10, "Tuna"),
+		MONKFISH(7946, 16, "Monkfish"),
+		SEA_TURTLE(397, 22, "Sea Turtle"),
+		TUNA_POTATO(7060, 22, "Tuna Potato");
 		
-		
-		private int id; private int heal; private String name;
+		private int id; 
+		private int heal; 
+		private String name;
 		
 		private FoodToEat(int id, int heal, String name) {
 			this.id = id;
@@ -46,15 +47,17 @@ public class Food {
 		public String getName() {
 			return name;
 		}
-		public static HashMap <Integer,FoodToEat> food = new HashMap<Integer,FoodToEat>();
+
+		public static HashMap<Integer, FoodToEat> food = new HashMap<>();
 		
 		public static FoodToEat forId(int id) {
 			return food.get(id);
 		}
 		
 		static {
-		for (FoodToEat f : FoodToEat.values())
-			food.put(f.getId(), f);
+			for (FoodToEat f : FoodToEat.values()) {
+				food.put(f.getId(), f);
+			}
 		}
 	}
 	
@@ -67,12 +70,13 @@ public class Food {
 			c.getCombat().resetPlayerAttack();
 			c.attackTimer += 2;
 			c.startAnimation(829);
-			c.getItems().deleteItem(id,slot,1);
+			c.getItems().deleteItem(id, slot, 1);
 			FoodToEat f = FoodToEat.food.get(id);
 			if (c.playerLevel[3] < c.getLevelForXP(c.playerXP[3])) {
 				c.playerLevel[3] += f.getHeal();
-				if (c.playerLevel[3] > c.getLevelForXP(c.playerXP[3]))
+				if (c.playerLevel[3] > c.getLevelForXP(c.playerXP[3])) {
 					c.playerLevel[3] = c.getLevelForXP(c.playerXP[3]);
+				}
 			}
 			c.foodDelay = System.currentTimeMillis();
 			c.getPA().refreshSkill(3);
@@ -80,10 +84,7 @@ public class Food {
 		}		
 	}
 
-	
 	public boolean isFood(int id) {
 		return FoodToEat.food.containsKey(id);
 	}	
-	
-
 }
