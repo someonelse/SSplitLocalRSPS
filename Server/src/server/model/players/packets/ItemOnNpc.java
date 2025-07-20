897d0bde4bf7313b28d094fb3ae83886934ec777
someonelse <someonelse13@gmail.com>
2025-07-19 23:34:02 -0400

    Update ItemOnNpc.java

--- Server/src/server/model/players/packets/ItemOnNpc.java
+++ Server/src/server/model/players/packets/ItemOnNpc.java
@@ -5,16 +5,23 @@ import server.model.items.UseItem;
 import server.model.players.Client;
 import server.model.players.PacketType;
 
-
+/**
+ * Handles when an item is used on an NPC.
+ * For example: using bones on an altar NPC or giving quest items.
+ */
 public class ItemOnNpc implements PacketType {
 
-	@Override
-	public void processPacket(Client c, int packetType, int packetSize) {
-		int itemId = c.getInStream().readSignedWordA();
-		int i = c.getInStream().readSignedWordA();
-		int slot = c.getInStream().readSignedWordBigEndian();
-		int npcId = Server.npcHandler.npcs[i].npcType;
-		
-		UseItem.ItemonNpc(c, itemId, npcId, slot);
-	}
+    @Override
+    public void processPacket(Client c, int packetType, int packetSize) {
+        int itemId = c.getInStream().readSignedWordA();
+        int npcIndex = c.getInStream().readSignedWordA();
+        int itemSlot = c.getInStream().readSignedWordBigEndian();
+
+        if (npcIndex < 0 || npcIndex >= Server.npcHandler.npcs.length || Server.npcHandler.npcs[npcIndex] == null) {
+            return;
+        }
+
+        int npcId = Server.npcHandler.npcs[npcIndex].npcType;
+        UseItem.ItemonNpc(c, itemId, npcId, itemSlot);
+    }
 }
