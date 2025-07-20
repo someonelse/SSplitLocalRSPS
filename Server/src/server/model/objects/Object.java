f55e20a2caaf1d99c3fcbee65021b36a448a2f52
someonelse <someonelse13@gmail.com>
2025-07-20 00:12:54 -0400

    Update Object.java

--- Server/src/server/model/objects/Object.java
+++ Server/src/server/model/objects/Object.java
@@ -2,18 +2,18 @@ package server.model.objects;
 
 import server.Server;
 
-public class Object {
-	
-	public int objectId;
-	public int objectX;
-	public int objectY;
-	public int height;
-	public int face;
-	public int type;
-	public int newId;
-	public int tick;
-	
-	public Object(int id, int x, int y, int height, int face, int type, int newId, int ticks) {
+public class GameObject {
+
+	private final int objectId;
+	private final int objectX;
+	private final int objectY;
+	private final int height;
+	private final int face;
+	private final int type;
+	private final int newId;
+	private final int tick;
+
+	public GameObject(int id, int x, int y, int height, int face, int type, int newId, int ticks) {
 		this.objectId = id;
 		this.objectX = x;
 		this.objectY = y;
@@ -25,5 +25,35 @@ public class Object {
 		Server.objectManager.addObject(this);
 	}
 
+	public int getObjectId() {
+		return objectId;
+	}
 
+	public int getObjectX() {
+		return objectX;
+	}
+
+	public int getObjectY() {
+		return objectY;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getFace() {
+		return face;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public int getNewId() {
+		return newId;
+	}
+
+	public int getTick() {
+		return tick;
+	}
 }
