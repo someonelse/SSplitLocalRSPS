4bd9548a7a8d8558f954ffcb4993e554e1726bdc
someonelse <someonelse13@gmail.com>
2025-07-19 21:55:09 -0400

    Update Clan.java

--- Server/src/server/world/Clan.java
+++ Server/src/server/world/Clan.java
@@ -2,19 +2,39 @@ package server.world;
 
 import server.model.players.Client;
 
-/**
- * @author Sanity
- */
-
 public class Clan {
 
-	public Clan(Client c, String name) {
-		this.owner = c.playerName;
-		this.name = name;
-	}
-		
-	public int[] members = new int [50];
-	public String name;
-	public String owner;
-	public boolean lootshare;
+    private final String owner;
+    private String name;
+    private final int[] members = new int[50];
+    private boolean lootshare;
+
+    public Clan(Client c, String name) {
+        this.owner = c.playerName;
+        this.name = name;
+    }
+
+    public int[] getMembers() {
+        return members;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public boolean isLootshare() {
+        return lootshare;
+    }
+
+    public void setLootshare(boolean lootshare) {
+        this.lootshare = lootshare;
+    }
 }
