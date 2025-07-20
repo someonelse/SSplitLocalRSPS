b81b9365d1286086d7aca831afac5280a1de4f60
someonelse <someonelse13@gmail.com>
2025-07-20 17:23:22 -0400

    Update CacheDownloader.java

--- Client/CacheDownloader.java
+++ Client/CacheDownloader.java
@@ -19,180 +19,167 @@ import sign.signlink;
 
 public class CacheDownloader {
 
-	private client client;
+    private final client client;
 
-	private final int BUFFER = 1024;
+    private static final int BUFFER = 1024;
 
-	private final int VERSION = 1; // Version of cache
-	private String cacheLink = "http://soulsplit.com/SoulSplit1.zip"; // Link to cache
+    private static final int VERSION = 1; // Version of cache
+    private static final String cacheLink = "http://soulsplit.com/SoulSplit1.zip"; // Link to cache
 
-	private String fileToExtract = getCacheDir() + getArchivedName();
+    private final String fileToExtract = getCacheDir() + getArchivedName();
 
-	public CacheDownloader(client client) {
-		this.client = client;
-	}
+    public CacheDownloader(client client) {
+        this.client = client;
+    }
 
-	private void drawLoadingText(String text) {
-		client.drawLoadingText(35, text);
-		//System.out.println(text);
-	}
+    private void drawLoadingText(String text) {
+        client.drawLoadingText(35, text);
+        //System.out.println(text);
+    }
 
+    private void drawLoadingText(int amount, String text) {
+        client.drawLoadingText(amount, text);
+        //System.out.println(text);
+    }
 
-	private void drawLoadingText(int amount, String text) {
-		client.drawLoadingText(amount, text);
-		//System.out.println(text);
-	}
+    private String getCacheDir() {
+        return signlink.findcachedir();
+    }
 
-	private String getCacheDir() {
-		return signlink.findcachedir();
-	}
+    private String getCacheLink() {
+        return cacheLink;
+    }
 
-	private String getCacheLink() {
-		return cacheLink;
-	}
+    private int getCacheVersion() {
+        return VERSION;
+    }
 
-	private int getCacheVersion() {
-		return VERSION;
-	}
+    public CacheDownloader downloadCache() {
+        try {
+            File location = new File(getCacheDir());
+            File version = new File(getCacheDir() + "/cacheVersion" + getCacheVersion() + ".dat");
 
-	public CacheDownloader downloadCache() {
-		try {
-		File location = new File(getCacheDir());
-		File version = new File(getCacheDir() + "/cacheVersion" + getCacheVersion() + ".dat");
-		
-		if(!location.exists()) {
-			//drawLoadingText("Loading new Updates....");
-			downloadFile(getCacheLink(), getArchivedName());
+            if (!location.exists()) {
+                //drawLoadingText("Loading new Updates....");
+                downloadFile(getCacheLink(), getArchivedName());
 
-			unZip();
-			//System.out.println("UNZIP");
+                unZip();
+                //System.out.println("UNZIP");
 
-			BufferedWriter versionFile = new BufferedWriter(new FileWriter(getCacheDir() + "/cacheVersion" + getCacheVersion() + ".dat"));
-			versionFile.close();
-		} else {
-			if(!version.exists()) {
-				//drawLoadingText("Downloading Cache Please wait...");
-				downloadFile(getCacheLink(), getArchivedName());
+                try (BufferedWriter versionFile = new BufferedWriter(new FileWriter(getCacheDir() + "/cacheVersion" + getCacheVersion() + ".dat"))) {
+                    // Create the version file and close it
+                }
+            } else {
+                if (!version.exists()) {
+                    //drawLoadingText("Downloading Cache Please wait...");
+                    downloadFile(getCacheLink(), getArchivedName());
 
-				unZip();
-				//System.out.println("UNZIP");
+                    unZip();
+                    //System.out.println("UNZIP");
 
-				BufferedWriter versionFile = new BufferedWriter(new FileWriter(getCacheDir() + "/cacheVersion" + getCacheVersion() + ".dat"));
-				versionFile.close();
+                    try (BufferedWriter versionFile = new BufferedWriter(new FileWriter(getCacheDir() + "/cacheVersion" + getCacheVersion() + ".dat"))) {
+                        // Create the version file and close it
+                    }
+                } else {
+                    return null;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-			} else {
-				return null;
-			}
-		}
-		} catch(Exception e) {
+    private void downloadFile(String adress, String localFileName) {
+        OutputStream out = null;
+        URLConnection conn = null;
+        InputStream in = null;
 
-		}
-		return null;
-	}
-	
-	private void downloadFile(String adress, String localFileName) {
-		OutputStream out = null;
-		URLConnection conn;
-		InputStream in = null;
-		
-		try {
+        try {
+            URL url = new URL(adress);
+            out = new BufferedOutputStream(
+                    new FileOutputStream(getCacheDir() + "/" + localFileName));
 
-			URL url = new URL(adress);
-			out = new BufferedOutputStream(
-				new FileOutputStream(getCacheDir() + "/" +localFileName)); 
+            conn = url.openConnection();
+            in = conn.getInputStream();
 
-			conn = url.openConnection();
-			in = conn.getInputStream(); 
-		
-			byte[] data = new byte[BUFFER]; 
-	
-			int numRead;
-			long numWritten = 0;
-			int length = conn.getContentLength();
+            byte[] data = new byte[BUFFER];
 
-	
-			while((numRead = in.read(data)) != -1) {
-				out.write(data, 0, numRead);
-				numWritten += numRead;
+            int numRead;
+            long numWritten = 0;
+            int length = conn.getContentLength();
 
-            			int percentage = (int)(((double)numWritten / (double)length) * 100D);
-				drawLoadingText(percentage, "Downloading Cache " + percentage + "%");
+            while ((numRead = in.read(data)) != -1) {
+                out.write(data, 0, numRead);
+                numWritten += numRead;
 
-			}
+                int percentage = (length > 0) ? (int) (((double) numWritten / (double) length) * 100D) : 0;
+                drawLoadingText(percentage, "Downloading Cache " + percentage + "%");
+            }
 
-			System.out.println(localFileName + "\t" + numWritten);
-			drawLoadingText("Finished downloading "+getArchivedName()+"!");
+            System.out.println(localFileName + "\t" + numWritten);
+            drawLoadingText("Finished downloading " + getArchivedName() + "!");
 
-		} catch (Exception exception) {
-			exception.printStackTrace();
-		} finally {
-			try {
-				if (in != null) {
-					in.close();
-				}
-				if (out != null) {
-					out.close();
-				}
-			} catch (IOException ioe) {
-			}
-		}
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
+    }
 
-	}
+    private String getArchivedName() {
+        int lastSlashIndex = getCacheLink().lastIndexOf('/');
+        if (lastSlashIndex >= 0
+                && lastSlashIndex < getCacheLink().length() - 1) {
+            return getCacheLink().substring(lastSlashIndex + 1);
+        } else {
+            //System.err.println("error retreiving archivaed name.");
+        }
+        return "";
+    }
 
-	private String getArchivedName() {
-		int lastSlashIndex = getCacheLink().lastIndexOf('/');
-		if (lastSlashIndex >= 0 
-			&& lastSlashIndex < getCacheLink().length() -1) { 
-			return getCacheLink().substring(lastSlashIndex + 1);
-		} else {
-			//System.err.println("error retreiving archivaed name.");
-		}
-		return "";
-	}
+    private void unZip() {
+        try (
+            InputStream in = new BufferedInputStream(new FileInputStream(fileToExtract));
+            ZipInputStream zin = new ZipInputStream(in)
+        ) {
+            ZipEntry e;
+            while ((e = zin.getNextEntry()) != null) {
+                if (e.isDirectory()) {
+                    new File(getCacheDir() + e.getName()).mkdir();
+                } else {
+                    if (e.getName().equals(fileToExtract)) {
+                        unzip(zin, fileToExtract);
+                        break;
+                    }
+                    unzip(zin, getCacheDir() + e.getName());
+                }
+                //System.out.println("unzipping2 " + e.getName());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    private void unzip(ZipInputStream zin, String s)
+            throws IOException {
 
-
-
-	private void unZip() {
-
-		try {
-    			InputStream in = 
-				new BufferedInputStream(new FileInputStream(fileToExtract));
-			ZipInputStream zin = new ZipInputStream(in);
-			ZipEntry e;
-
-			while((e=zin.getNextEntry()) != null) {
-
-               			if(e.isDirectory()) {
-					(new File(getCacheDir() + e.getName())).mkdir();
-               			} else {
-
-				if (e.getName().equals(fileToExtract)) {
-					unzip(zin, fileToExtract);
-					break;
-				}
-       					unzip(zin, getCacheDir() + e.getName());
-				}
-				//System.out.println("unzipping2 " + e.getName());
-			}
-			zin.close();
-
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void unzip(ZipInputStream zin, String s) 
-		throws IOException {
-
-		FileOutputStream out = new FileOutputStream(s);
-		//System.out.println("unzipping " + s);
-		byte [] b = new byte[BUFFER];
-		int len = 0;
-
-		while ((len = zin.read(b)) != -1) {
-			out.write(b,0,len);
-		}
-		out.close();
-	}
+        try (FileOutputStream out = new FileOutputStream(s)) {
+            //System.out.println("unzipping " + s);
+            byte[] b = new byte[BUFFER];
+            int len;
+            while ((len = zin.read(b)) != -1) {
+                out.write(b, 0, len);
+            }
+        }
+    }
 }
