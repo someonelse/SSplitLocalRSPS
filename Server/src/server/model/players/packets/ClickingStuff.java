2d09757651521d0c734dc305b7e3bd8b6c68b18e
someonelse <someonelse13@gmail.com>
2025-07-19 23:52:58 -0400

    Update ClickingStuff.java

--- Server/src/server/model/players/packets/ClickingStuff.java
+++ Server/src/server/model/players/packets/ClickingStuff.java
@@ -5,36 +5,37 @@ import server.model.players.Client;
 import server.model.players.PacketType;
 import server.util.Misc;
 
-
 /**
  * Clicking stuff (interfaces)
  **/
 public class ClickingStuff implements PacketType {
 
-	@Override
-	public void processPacket(Client c, int packetType, int packetSize) {
-		if (c.inTrade) {
-			if(!c.acceptedTrade) {
-				Misc.println("trade reset");
-				c.getTradeAndDuel().declineTrade();
-			}
-		}
-		
-		if (c.isBanking)
-		c.isBanking = false;
+    @Override
+    public void processPacket(Client c, int packetType, int packetSize) {
+        // Decline trade if in trade and not yet accepted
+        if (c.inTrade) {
+            if (!c.acceptedTrade) {
+                Misc.println("trade reset");
+                c.getTradeAndDuel().declineTrade();
+            }
+        }
 
-		Client o = (Client) Server.playerHandler.players[c.duelingWith];
-		if(o != null) {
-			if(c.duelStatus >= 1 && c.duelStatus <= 4) {
-				c.getTradeAndDuel().declineDuel();
-				o.getTradeAndDuel().declineDuel();
-			}
-		}
-		
-		if(c.duelStatus == 6) {
-			c.getTradeAndDuel().claimStakedItems();		
-		}
-	
-	}
-		
+        // Exit banking mode
+        if (c.isBanking)
+            c.isBanking = false;
+
+        // Duel interaction
+        Client o = (Client) Server.playerHandler.players[c.duelingWith];
+        if (o != null) {
+            if (c.duelStatus >= 1 && c.duelStatus <= 4) {
+                c.getTradeAndDuel().declineDuel();
+                o.getTradeAndDuel().declineDuel();
+            }
+        }
+
+        // Claim staked items if duel status is complete
+        if (c.duelStatus == 6) {
+            c.getTradeAndDuel().claimStakedItems();
+        }
+    }
 }
