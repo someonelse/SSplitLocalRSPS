5b5bce87852d4a1226c2f6e45ebf52da91410f19
someonelse <someonelse13@gmail.com>
2025-07-20 00:09:00 -0400

    Update Bank5.java

--- Server/src/server/model/players/packets/Bank5.java
+++ Server/src/server/model/players/packets/Bank5.java
@@ -2,6 +2,7 @@ package server.model.players.packets;
 
 import server.model.players.Client;
 import server.model.players.PacketType;
+
 /**
  * Bank 5 Items
  **/
@@ -9,58 +10,46 @@ public class Bank5 implements PacketType {
 
 	@Override
 	public void processPacket(Client c, int packetType, int packetSize) {
-	int interfaceId = c.getInStream().readSignedWordBigEndianA();
-	int removeId = c.getInStream().readSignedWordBigEndianA();
-	int removeSlot = c.getInStream().readSignedWordBigEndian();
-	
-	if (c.inTrade)
-	return;
-	
-		switch(interfaceId){
+		int interfaceId = c.getInStream().readSignedWordBigEndianA();
+		int removeId = c.getInStream().readSignedWordBigEndianA();
+		int removeSlot = c.getInStream().readSignedWordBigEndian();
 
-			case 3900:
-			c.getShops().buyItem(removeId, removeSlot, 1);
-			break;
-			
-			case 3823:
-			c.getShops().sellItem(removeId, removeSlot, 1);
-			break;
-			
-			case 5064:
-			c.getItems().bankItem(removeId, removeSlot, 5);
-			break;
-			
-			case 5382:
-			c.getItems().fromBank(removeId, removeSlot, 5);
-			break;
-			
-			case 3322:
-			if(c.duelStatus <= 0) { 
-                c.getTradeAndDuel().tradeItem(removeId, removeSlot, 5);
-           	} else {
-				c.getTradeAndDuel().stakeItem(removeId, removeSlot, 5);
-			}	
-			break;
-			
-			case 3415:
-			if(c.duelStatus <= 0) { 
-				c.getTradeAndDuel().fromTrade(removeId, removeSlot, 5);
+		if (c.inTrade) {
+			return;
+		}
+
+		switch (interfaceId) {
+			case 3900 -> // Buy from shop
+				c.getShops().buyItem(removeId, removeSlot, 1);
+
+			case 3823 -> // Sell to shop
+				c.getShops().sellItem(removeId, removeSlot, 1);
+
+			case 5064 -> // Inventory to bank
+				c.getItems().bankItem(removeId, removeSlot, 5);
+
+			case 5382 -> // From bank to inventory
+				c.getItems().fromBank(removeId, removeSlot, 5);
+
+			case 3322 -> { // Trade or duel
+				if (c.duelStatus <= 0) {
+					c.getTradeAndDuel().tradeItem(removeId, removeSlot, 5);
+				} else {
+					c.getTradeAndDuel().stakeItem(removeId, removeSlot, 5);
+				}
 			}
-			break;
-			
-			case 6669:
-			c.getTradeAndDuel().fromDuel(removeId, removeSlot, 5);
-			break;
-			
-			case 1119:
-			case 1120:
-			case 1121:
-			case 1122:
-			case 1123:
+
+			case 3415 -> { // From trade window
+				if (c.duelStatus <= 0) {
+					c.getTradeAndDuel().fromTrade(removeId, removeSlot, 5);
+				}
+			}
+
+			case 6669 -> // From duel stake
+				c.getTradeAndDuel().fromDuel(removeId, removeSlot, 5);
+
+			case 1119, 1120, 1121, 1122, 1123 -> // Smithing interface
 				c.getSmithing().readInput(c.playerLevel[c.playerSmithing], Integer.toString(removeId), c, 5);
-			break;
-			
 		}
 	}
-
 }
