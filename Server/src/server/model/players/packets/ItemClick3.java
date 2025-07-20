1991fa1660c8a675b912f193ac5d1d9e2ed75479
someonelse <someonelse13@gmail.com>
2025-07-19 23:35:42 -0400

    Update ItemClick3.java

--- Server/src/server/model/players/packets/ItemClick3.java
+++ Server/src/server/model/players/packets/ItemClick3.java
@@ -5,34 +5,38 @@ import server.model.players.PacketType;
 import server.util.Misc;
 
 /**
- * Item Click 3 Or Alternative Item Option 1
+ * Handles the third click action on an inventory item,
+ * also known as "Item Option 3" or "Alternative Item Option 1".
+ * 
+ * Example use: glory teleport (rub)
  * 
  * @author Ryan / Lmctruck30
- * 
- * Proper Streams
  */
-
 public class ItemClick3 implements PacketType {
 
-	@Override
-	public void processPacket(Client c, int packetType, int packetSize) {
-		int itemId11 = c.getInStream().readSignedWordBigEndianA();
-		int itemId1 = c.getInStream().readSignedWordA();
-		int itemId = c.getInStream().readSignedWordA();
-		
+    @Override
+    public void processPacket(Client c, int packetType, int packetSize) {
+        // Read stream data
+        int dummy1 = c.getInStream().readSignedWordBigEndianA();
+        int dummy2 = c.getInStream().readSignedWordA();
+        int itemId = c.getInStream().readSignedWordA();
 
-		switch (itemId) {
+        // Handle special item behavior
+        switch (itemId) {
+            case 1712: // Amulet of Glory
+            case 1706: // Glory (5)
+            case 1708: // Glory (4)
+            case 1710: // Glory (3)
+            case 1712: // Glory (2)
+                c.getPA().handleGlory(itemId);
+                break;
 
-		case 1712:
-			c.getPA().handleGlory(itemId);
-			break;
-			
-		default:
-			if (c.playerRights == 3)
-				Misc.println(c.playerName+ " - Item3rdOption: "+itemId+" : "+itemId11+" : "+itemId1);
-			break;
-		}
-
-	}
-
+            default:
+                if (c.playerRights == 3) {
+                    Misc.println("DEBUG: " + c.playerName + " used Item 3rd Option on item ID: " + itemId +
+                                 " [Extra: " + dummy1 + ", " + dummy2 + "]");
+                }
+                break;
+        }
+    }
 }
