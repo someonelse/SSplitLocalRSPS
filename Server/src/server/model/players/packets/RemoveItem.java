bb59f516386a1d53e469e3575383038ee33698d0
someonelse <someonelse13@gmail.com>
2025-07-19 23:29:27 -0400

    Update RemoveItem.java

--- Server/src/server/model/players/packets/RemoveItem.java
+++ Server/src/server/model/players/packets/RemoveItem.java
@@ -3,69 +3,64 @@ package server.model.players.packets;
 import server.model.players.Client;
 import server.model.players.PacketType;
 
-
 /**
- * Remove Item
- **/
+ * Handles removal of an item from various game interfaces.
+ * This includes inventory, bank, shops, trading, dueling, and smithing interfaces.
+ */
 public class RemoveItem implements PacketType {
 
-	@Override
-	public void processPacket(Client c, int packetType, int packetSize) {
-		int interfaceId = c.getInStream().readUnsignedWordA();
-		int removeSlot = c.getInStream().readUnsignedWordA();
-		int removeId = c.getInStream().readUnsignedWordA();
-		int shop = 0;
-		int value = 0;
-		String name = "null";
-	
-		switch(interfaceId) {
-			
-			case 1688:
-			c.getItems().removeItem(removeId, removeSlot);
-			break;
-			
-			case 5064:
-			c.getItems().bankItem(removeId, removeSlot, 1);
-			break;
-			
-			case 5382:
-			c.getItems().fromBank(removeId, removeSlot, 1);
-			break;
-			
-			case 3900:
-			c.getShops().buyFromShopPrice(removeId, removeSlot);
-			break;
-			
-			case 3823:
-			c.getShops().sellToShopPrice(removeId, removeSlot);
-			break;
-			
-			case 3322:
-			if(c.duelStatus <= 0) { 
-                c.getTradeAndDuel().tradeItem(removeId, removeSlot, 1);
-           	} else {
-				c.getTradeAndDuel().stakeItem(removeId, removeSlot, 1);
-			}
-			break;
-			
-			case 3415:
-			if(c.duelStatus <= 0) { 
-				c.getTradeAndDuel().fromTrade(removeId, removeSlot, 1);
-           	} 
-			break;
-			
-			case 6669:
-			c.getTradeAndDuel().fromDuel(removeId, removeSlot, 1);
-			break;
-			
-			case 1119:
-			case 1120:
-			case 1121:
-			case 1122:
-			case 1123:
-				c.getSmithing().readInput(c.playerLevel[c.playerSmithing], Integer.toString(removeId), c, 1);
-			break;
-		}
-	}
-			
+    @Override
+    public void processPacket(Client c, int packetType, int packetSize) {
+        int interfaceId = c.getInStream().readUnsignedWordA();
+        int removeSlot = c.getInStream().readUnsignedWordA();
+        int removeId = c.getInStream().readUnsignedWordA();
+
+        switch (interfaceId) {
+            case 1688: // Equipment tab
+                c.getItems().removeItem(removeId, removeSlot);
+                break;
+
+            case 5064: // Inventory to bank
+                c.getItems().bankItem(removeId, removeSlot, 1);
+                break;
+
+            case 5382: // Bank to inventory
+                c.getItems().fromBank(removeId, removeSlot, 1);
+                break;
+
+            case 3900: // Shop value (buy)
+                c.getShops().buyFromShopPrice(removeId, removeSlot);
+                break;
+
+            case 3823: // Shop value (sell)
+                c.getShops().sellToShopPrice(removeId, removeSlot);
+                break;
+
+            case 3322: // Trade or Duel stake
+                if (c.duelStatus <= 0) {
+                    c.getTradeAndDuel().tradeItem(removeId, removeSlot, 1);
+                } else {
+                    c.getTradeAndDuel().stakeItem(removeId, removeSlot, 1);
+                }
+                break;
+
+            case 3415: // From trade
+                if (c.duelStatus <= 0) {
+                    c.getTradeAndDuel().fromTrade(removeId, removeSlot, 1);
+                }
+                break;
+
+            case 6669: // From duel
+                c.getTradeAndDuel().fromDuel(removeId, removeSlot, 1);
+                break;
+
+            case 1119: // Smithing interface
+            case 1120:
+            case 1121:
+            case 1122:
+            case 1123:
+                c.getSmithing().readInput(c.playerLevel[c.playerSmithing], Integer.toString(removeId), c, 1);
+                break;
+        }
+    }
 }
