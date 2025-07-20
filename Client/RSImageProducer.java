c404f10b8e6c2132773ae65d1063c1a6e4c9fd81
someonelse <someonelse13@gmail.com>
2025-07-20 15:16:23 -0400

    Update RSImageProducer.java

--- Client/RSImageProducer.java
+++ Client/RSImageProducer.java
@@ -5,85 +5,105 @@
 import java.awt.*;
 import java.awt.image.*;
 
-final class RSImageProducer
-		implements ImageProducer, ImageObserver
-{
+/**
+ * RSImageProducer is responsible for producing images used by the client.
+ * It implements both ImageProducer and ImageObserver to allow for drawing
+ * and updating the image buffer.
+ */
+final class RSImageProducer implements ImageProducer, ImageObserver {
 
-	public RSImageProducer(int i, int j, Component component)
-	{
-		anInt316 = i;
-		anInt317 = j;
-		anIntArray315 = new int[i * j];
-		aColorModel318 = new DirectColorModel(32, 0xff0000, 65280, 255);
-		anImage320 = component.createImage(this);
-		method239();
-		component.prepareImage(anImage320, this);
-		method239();
-		component.prepareImage(anImage320, this);
-		method239();
-		component.prepareImage(anImage320, this);
-		initDrawingArea();
-	}
+    public RSImageProducer(int width, int height, Component component) {
+        anInt316 = width;
+        anInt317 = height;
+        anIntArray315 = new int[width * height];
+        aColorModel318 = new DirectColorModel(32, 0xff0000, 0x00ff00, 0x0000ff);
+        anImage320 = component.createImage(this);
+        
+        // Prepare the image multiple times to ensure compatibility
+        method239();
+        component.prepareImage(anImage320, this);
+        method239();
+        component.prepareImage(anImage320, this);
+        method239();
+        component.prepareImage(anImage320, this);
 
-	public void initDrawingArea()
-	{
-		DrawingArea.initDrawingArea(anInt317, anInt316, anIntArray315);
-	}
+        initDrawingArea();
+    }
 
-	public void drawGraphics(int i, Graphics g, int k)
-	{
-		method239();
-		g.drawImage(anImage320, k, i, this);
-	}
+    /**
+     * Initializes the drawing area with current buffer and dimensions.
+     */
+    public void initDrawingArea() {
+        DrawingArea.initDrawingArea(anInt317, anInt316, anIntArray315);
+    }
 
-	public synchronized void addConsumer(ImageConsumer imageconsumer)
-	{
-		anImageConsumer319 = imageconsumer;
-		imageconsumer.setDimensions(anInt316, anInt317);
-		imageconsumer.setProperties(null);
-		imageconsumer.setColorModel(aColorModel318);
-		imageconsumer.setHints(14);
-	}
+    /**
+     * Draws the buffered image onto the specified graphics context.
+     * 
+     * @param y Vertical offset
+     * @param g Graphics context
+     * @param x Horizontal offset
+     */
+    public void drawGraphics(int y, Graphics g, int x) {
+        method239();
+        g.drawImage(anImage320, x, y, this);
+    }
 
-	public synchronized boolean isConsumer(ImageConsumer imageconsumer)
-	{
-		return anImageConsumer319 == imageconsumer;
-	}
+    @Override
+    public synchronized void addConsumer(ImageConsumer consumer) {
+        anImageConsumer319 = consumer;
+        consumer.setDimensions(anInt316, anInt317);
+        consumer.setProperties(null);
+        consumer.setColorModel(aColorModel318);
+        consumer.setHints(ImageConsumer.TOPDOWNLEFTRIGHT | ImageConsumer.COMPLETESCANLINES | ImageConsumer.SINGLEPASS);
+    }
 
-	public synchronized void removeConsumer(ImageConsumer imageconsumer)
-	{
-		if(anImageConsumer319 == imageconsumer)
-			anImageConsumer319 = null;
-	}
+    @Override
+    public synchronized boolean isConsumer(ImageConsumer consumer) {
+        return anImageConsumer319 == consumer;
+    }
 
-	public void startProduction(ImageConsumer imageconsumer)
-	{
-		addConsumer(imageconsumer);
-	}
+    @Override
+    public synchronized void removeConsumer(ImageConsumer consumer) {
+        if (anImageConsumer319 == consumer) {
+            anImageConsumer319 = null;
+        }
+    }
 
-	public void requestTopDownLeftRightResend(ImageConsumer imageconsumer)
-	{
-		System.out.println("TDLR");
-	}
+    @Override
+    public void startProduction(ImageConsumer consumer) {
+        addConsumer(consumer);
+    }
 
-	private synchronized void method239()
-	{
-		if(anImageConsumer319 != null)
-		{
-			anImageConsumer319.setPixels(0, 0, anInt316, anInt317, aColorModel318, anIntArray315, 0, anInt316);
-			anImageConsumer319.imageComplete(2);
-		}
-	}
+    @Override
+    public void requestTopDownLeftRightResend(ImageConsumer consumer) {
+        System.out.println("TDLR");
+    }
 
-	public boolean imageUpdate(Image image, int i, int j, int k, int l, int i1)
-	{
-		return true;
-	}
+    /**
+     * Notifies the consumer to refresh its pixels.
+     */
+    private synchronized void method239() {
+        if (anImageConsumer319 != null) {
+            anImageConsumer319.setPixels(
+                0, 0, anInt316, anInt317,
+                aColorModel318,
+                anIntArray315, 0, anInt316
+            );
+            anImageConsumer319.imageComplete(ImageConsumer.STATICIMAGEDONE);
+        }
+    }
 
-	public final int[] anIntArray315;
-	private final int anInt316;
-	private final int anInt317;
-	private final ColorModel aColorModel318;
-	private ImageConsumer anImageConsumer319;
-	private final Image anImage320;
+    @Override
+    public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height) {
+        return true;
+    }
+
+    // Fields
+    public final int[] anIntArray315;
+    private final int anInt316;
+    private final int anInt317;
+    private final ColorModel aColorModel318;
+    private ImageConsumer anImageConsumer319;
+    private final Image anImage320;
 }
