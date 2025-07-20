23c077bebd64133aaeb7559694c95df818f3b446
someonelse <someonelse13@gmail.com>
2025-07-19 22:40:36 -0400

    Update PlayerKilling.java

--- Server/src/server/model/players/PlayerKilling.java
+++ Server/src/server/model/players/PlayerKilling.java
@@ -2,46 +2,42 @@ package server.model.players;
 
 public class PlayerKilling {
 
-	private Client c;
-	
-	/**
-	* Constructor class
-	*/
-	
-	public PlayerKilling(Client Client) {
-		this.c = Client;
-	}
-	
-	/**
-	* How many people you have to kill before getting points again
-	* for killing the same person.
-	*/
-	
-	public final int NEEDED_KILLS = 10;
-	
-	/**
-	* First the method checks if the array list contains the person
-	* and if it doesn't then add there name but if it does then
-	* return the method false.
-	*/
-	
-	public boolean addPlayer(String i) {
-		if(!c.killedPlayers.contains(i)) {
-			c.killedPlayers.add(i);
-			return true;
-		}
-		return false;
-	}
-	
-	/**
-	* Checking if the array list contains the player and if
-	* the person has killed 20 or more people since that person.
-	*/
-	
-	public void checkForPlayer(String i) {
-		if(c.killedPlayers.contains(i) && c.killedPlayers.indexOf(i) >= NEEDED_KILLS) {
-			c.killedPlayers.remove(i);
-		}
-	}
+    private Client c;
 
+    /** 
+     * How many people you have to kill before getting points again
+     * for killing the same person.
+     */
+    public static final int NEEDED_KILLS = 10;
+
+    /**
+     * Constructor
+     */
+    public PlayerKilling(Client c) {
+        this.c = c;
+    }
+
+    /**
+     * Attempts to add a player name to the killedPlayers list.
+     * @param name The name of the player killed.
+     * @return true if the name was not already in the list and was added; false otherwise.
+     */
+    public boolean addPlayer(String name) {
+        if (!c.killedPlayers.contains(name)) {
+            c.killedPlayers.add(name);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Removes a player from the killedPlayers list if they have been in it for more than NEEDED_KILLS.
+     * @param name The name of the player to check for.
+     */
+    public void checkForPlayer(String name) {
+        int index = c.killedPlayers.indexOf(name);
+        if (index >= NEEDED_KILLS) {
+            c.killedPlayers.remove(name);
+        }
+    }
 }
