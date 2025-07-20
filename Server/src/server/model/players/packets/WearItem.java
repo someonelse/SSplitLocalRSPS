5ebc2fe7dc84f31116db1148f393fe7335b9d5f1
someonelse <someonelse13@gmail.com>
2025-07-19 23:26:59 -0400

    Update WearItem.java

--- Server/src/server/model/players/packets/WearItem.java
+++ Server/src/server/model/players/packets/WearItem.java
@@ -3,37 +3,55 @@ package server.model.players.packets;
 import server.model.players.Client;
 import server.model.players.PacketType;
 
-
 /**
  * Wear Item
- **/
+ */
 public class WearItem implements PacketType {
 
-	@Override
-	public void processPacket(Client c, int packetType, int packetSize) {
-		c.wearId = c.getInStream().readUnsignedWord();
-		c.wearSlot = c.getInStream().readUnsignedWordA();
-		c.interfaceId = c.getInStream().readUnsignedWordA();
-		
-		int oldCombatTimer = c.attackTimer;
-		if (c.playerIndex > 0 || c.npcIndex > 0)
-			c.getCombat().resetPlayerAttack();
-		if (c.wearId >= 5509 && c.wearId <= 5515) {
-			int pouch = -1;
-			int a = c.wearId;
-			if (a == 5509)
-				pouch = 0;
-			if (a == 5510)
-				pouch = 1;
-			if (a == 5512)
-				pouch = 2;
-			if (a == 5514)
-				pouch = 3;
-			c.getPA().emptyPouch(pouch);
-			return;
-		}
-			//c.attackTimer = oldCombatTimer;
-		c.getItems().wearItem(c.wearId, c.wearSlot);
-	}
+    @Override
+    public void processPacket(Client c, int packetType, int packetSize) {
+        c.wearId = c.getInStream().readUnsignedWord();
+        c.wearSlot = c.getInStream().readUnsignedWordA();
+        c.interfaceId = c.getInStream().readUnsignedWordA();
 
+        // Prevent equipment during combat
+        if (c.playerIndex > 0 || c.npcIndex > 0) {
+            c.getCombat().resetPlayerAttack();
+        }
+
+        // Handle rune pouch usage
+        if (c.wearId >= 5509 && c.wearId <= 5515) {
+            int pouch = getPouchIndex(c.wearId);
+            if (pouch != -1) {
+                c.getPA().emptyPouch(pouch);
+            }
+            return;
+        }
+
+        // Basic validation (optional but good practice)
+        if (c.wearSlot < 0 || c.wearSlot >= c.playerEquipment.length) {
+            return;
+        }
+
+        if (c.wearId <= 0) {
+            return;
+        }
+
+        c.getItems().wearItem(c.wearId, c.wearSlot);
+    }
+
+    private int getPouchIndex(int itemId) {
+        switch (itemId) {
+            case 5509:
+                return 0;
+            case 5510:
+                return 1;
+            case 5512:
+                return 2;
+            case 5514:
+                return 3;
+            default:
+                return -1;
+        }
+    }
 }
