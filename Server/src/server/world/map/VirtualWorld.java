eaa81dc46b04ca8f93a6915543922fd3488405df
someonelse <someonelse13@gmail.com>
2025-07-19 21:56:12 -0400

    Update VirtualWorld.java

--- Server/src/server/world/map/VirtualWorld.java
+++ Server/src/server/world/map/VirtualWorld.java
@@ -3,46 +3,36 @@ package server.world.map;
 import java.util.Hashtable;
 import java.util.Map;
 
+public class VirtualWorld {
 
-public class VirtualWorld
-{
+    // Use generics for typesafety; Object for legacy compatibility.
+    public static final Map<Object, Object>[] maps = new Hashtable[10331];
 
-    public VirtualWorld()
-    {
-    }
+    public VirtualWorld() {}
 
-    public static final void main(String args[])
-    {
+    public static void main(String[] args) {
         init();
     }
 
-	public static final void init()
-    {
-        for(int i = 0; i < 10331; i++)
-        {
-            I[i] = null;
-            I[i] = new Hashtable();
+    public static void init() {
+        for (int i = 0; i < maps.length; i++) {
+            maps[i] = new Hashtable<>();
         }
 
         server.world.map.I.I(true);
-        for(int j = 0; j < 0x989680; j++)
+        for (int j = 0; j < 0x989680; j++) {
             I(0, 3222, 3242, 3223, 3243, 0);
-
+        }
     }
 
-    public static final boolean I(int height, int currentX, int currentY, int futureX, int futureY, int a)
-    {
-		if (height % 4 == 0)
-			height = 0;
-		if (currentX != futureX && futureY != currentY) {
-			return server.world.map.I.I(height, currentX, currentY, currentX, futureY, a) && server.world.map.I.I(height, currentX, currentY, futureX, currentY, a) 
-					&& server.world.map.I.I(height, currentX, currentY, futureX, futureY, a);
-		}
+    public static boolean I(int height, int currentX, int currentY, int futureX, int futureY, int a) {
+        if (height % 4 == 0)
+            height = 0;
+        if (currentX != futureX && futureY != currentY) {
+            return server.world.map.I.I(height, currentX, currentY, currentX, futureY, a)
+                && server.world.map.I.I(height, currentX, currentY, futureX, currentY, a)
+                && server.world.map.I.I(height, currentX, currentY, futureX, futureY, a);
+        }
         return server.world.map.I.I(height, currentX, currentY, futureX, futureY, a);
     }
-
-	public static Map I[] = new Hashtable[10331];
-
 }
-
-
